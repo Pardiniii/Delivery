50e8139c8449564bdac631a987b4d8a688d15540
gustavo.pardini <ggpardini@gmail.com>
2024-10-07 16:02:57 -0300

    Implementando Lazy nas columns e rows

--- app/src/main/java/br/com/alura/aluvery/ui/components/ProductItem.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/components/ProductItem.kt
@@ -1,8 +1,16 @@
 package br.com.alura.aluvery.ui.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
